bb7a0170482dae8851a4fee7a9fed84c68432797
强堃 <453421041@qq.com>
2020-04-05 11:10:06 +0800

    Initial

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const { name } = require("./package");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  outputDir: `./dist`,
+  assetsDir: "static",
+  filenameHashing: true,
+  devServer: {
+    host: "localhost",
+    port: 9000,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set("@", `${resolve("src")}`);
+    let oneOfsMap = config.module.rule("scss").oneOfs.store;
+    oneOfsMap.forEach(item => {
+      item
+        .use("sass-resources-loader")
+        .loader("sass-resources-loader")
+        .options({
+          resources: [
+            resolve("src/style/var.scss/var.scss"),
+            resolve("src/style/var.scss/mixins.scss")
+          ]
+        })
+        .end();
+    });
+  }
+};
